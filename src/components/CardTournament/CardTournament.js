23d82879b24ac61111588baff7346a0c45f1544b
Gussiny <a01635151@itesm.mx>
2021-10-11 22:11:02 -0500

    feat: create tournament card component

---
+++ src/components/CardTournament/CardTournament.js
@@ -0,0 +1,15 @@
+import "./CardTournament.scss";
+
+const CardTournament = () => {
+  return (
+    <div className="card tr-card mb-4">
+        <div className="card-body">
+            <h5 className="card-title"> 
+                Card
+            </h5>
+        </div>
+    </div>
+  );
+};
+
+export default CardTournament;
