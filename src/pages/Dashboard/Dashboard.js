96afe9761b52664cc6a9b31be93747177aec35b1
Erick <erick_tec@outlook.com>
2021-10-28 13:23:55 -0500

    feat: authentication to backend

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,25 +8,25 @@ import NavBar from '../../components/NavBar/NavBar';
 import CardTournament from '../../components/CardTournament/CardTournament';
 
 
-const Dashboard = () => {
+const Dashboard = ({ loggedIn, setLoggedIn }) => {
 
     return (
         <div>
-            <NavBar/>
+            <NavBar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
             <Container fluid>
                 <Row>
-                    <div className='box-header-gradient'>Torneos</div>    
+                    <div className='box-header-gradient'>Torneos</div>
                 </Row>
                 <Row>
                     <Container>
                         <CardTournament
-                            tournamentName = { 'Tepatitlan' }
-                            tournamentImage = { 'https://www.wallpapertip.com/wmimgs/31-315325_tennis-ball-sport-wallpaper-tennis-wallpapers-hd.jpg' }
+                            tournamentName={'Tepatitlan'}
+                            tournamentImage={'https://www.wallpapertip.com/wmimgs/31-315325_tennis-ball-sport-wallpaper-tennis-wallpapers-hd.jpg'}
                         />
 
                         <CardTournament
-                            tournamentName = { 'Lagos de Moreno' }
-                            tournamentImage = { 'https://images5.alphacoders.com/971/thumb-1920-971321.jpg' }
+                            tournamentName={'Lagos de Moreno'}
+                            tournamentImage={'https://images5.alphacoders.com/971/thumb-1920-971321.jpg'}
                         />
                     </Container>
                 </Row>
